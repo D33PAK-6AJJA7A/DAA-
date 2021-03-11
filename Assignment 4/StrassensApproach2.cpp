5f3dad9f5a2a87e373f2e4e3e37bd9057582566e
BISWAJEET DAS <me.athe1stb@gmail.com>
2021-03-11 13:23:14 +0530

    Create StrassensApproach2.cpp

---
+++ Assignment 4/StrassensApproach2.cpp
@@ -0,0 +1,125 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int** initializeMatrix(int n)
+{
+    int **matrix = (int **)malloc(n * sizeof(int *)); 
+    int row;
+
+    for (int i=0; i<n; i++)
+        matrix[i] = (int *)malloc(n * sizeof(int));
+
+    for(int i=0; i<n; i++)
+        for(int j=0; j<n; j++)
+            matrix[i][j]=0;
+
+    return matrix;
+}
+
+int** add(int** M1, int** M2, int n) {
+    int** temp = initializeMatrix(n);
+    for(int i=0; i<n; i++)
+        for(int j=0; j<n; j++)
+            temp[i][j] = M1[i][j] + M2[i][j];
+    return temp;
+}
+
+int** subtract(int** M1, int** M2, int n) {
+    int** temp = initializeMatrix(n);
+    for(int i=0; i<n; i++)
+        for(int j=0; j<n; j++)
+            temp[i][j] = M1[i][j] - M2[i][j];
+    return temp;
+}
+
+int** strassenMultiply(int** A, int**B , int n)
+{
+    if(n==1)
+    {
+        int** C = initializeMatrix(1);
+        C[0][0] =  A[0][0]*B[0][0];
+        return C;
+    }
+
+    int k = n/2;
+
+    int** C = initializeMatrix(n);
+    int** A11 = initializeMatrix(k);
+    int** A12 = initializeMatrix(k);
+    int** A21 = initializeMatrix(k);
+    int** A22 = initializeMatrix(k);
+    int** B11 = initializeMatrix(k);
+    int** B12 = initializeMatrix(k);
+    int** B21 = initializeMatrix(k);
+    int** B22 = initializeMatrix(k);
+
+    for(int i=0; i<k; i++)
+    {
+        for(int j=0; j<k; j++) 
+        {
+            A11[i][j] = A[i][j];
+            A12[i][j] = A[i][k+j];
+            A21[i][j] = A[k+i][j];
+            A22[i][j] = A[k+i][k+j];
+            B11[i][j] = B[i][j];
+            B12[i][j] = B[i][k+j];
+            B21[i][j] = B[k+i][j];
+            B22[i][j] = B[k+i][k+j];
+        }
+    }
+
+    int** P1 = strassenMultiply(A11, subtract(B12, B22, k), k);
+    int** P2 = strassenMultiply(add(A11, A12, k), B22, k);
+    int** P3 = strassenMultiply(add(A21, A22, k), B11, k);
+    int** P4 = strassenMultiply(A22, subtract(B21, B11, k), k);
+    int** P5 = strassenMultiply(add(A11, A22, k), add(B11, B22, k), k);
+    int** P6 = strassenMultiply(subtract(A12, A22, k), add(B21, B22, k), k);
+    int** P7 = strassenMultiply(subtract(A11, A21, k), add(B11, B12, k), k);
+
+    int** C11 = subtract(add(add(P5, P4, k), P6, k), P2, k);
+    int** C12 = add(P1, P2, k);
+    int** C21 = add(P3, P4, k);
+    int** C22 = subtract(subtract(add(P5, P1, k), P3, k), P7, k);
+
+    for(int i=0; i<k; i++)
+    {
+        for(int j=0; j<k; j++) 
+        {
+            C[i][j] = C11[i][j];
+            C[i][j+k] = C12[i][j];
+            C[k+i][j] = C21[i][j];
+            C[k+i][k+j] = C22[i][j];
+        }
+    }
+
+    return C;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);    cin.tie(NULL);    cout.tie(NULL);
+
+    int n=2;
+
+    int** A = initializeMatrix(n); 
+    int** B = initializeMatrix(n);
+
+    for(int i=0; i<n; i++)
+        for(int j=0; j<n; j++)
+            cin>>A[i][j];
+
+    for(int i=0; i<n; i++)
+        for(int j=0; j<n; j++)
+            cin>>B[i][j];
+
+    int** ans = strassenMultiply(A,B,n);
+
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<n; j++)
+            cout<<ans[i][j]<<"\t";
+        cout<<"\n";
+    }
+
+    return 0;
+}
